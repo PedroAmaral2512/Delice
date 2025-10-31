0f35a04495218b4b0f22e58bbffa3834f39baf55
PedroAamral2512 <pedroenrique25pro@gmail.com>
2025-10-31 20:31:58 -0300

    ...

--- Delice.API/Models/Usuario.cs
+++ Delice.API/Models/Usuario.cs
@@ -1,9 +1,18 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+namespace GCook.Models;
 
-namespace Delice.API.Models;
+[Table("Usuario")]
+public class Usuario : IdentityUser
+{
+    [Required]
+    [StringLength(50)]
+    public string Nome { get; set; }
 
-    public class Usuario
-    {
-        
-    }
+    [DataType(DataType.Date)]
+    public DateTime? DataNascimento { get; set; }
+
+    [StringLength(300)]
+    public string Foto { get; set; }
+}
